4804626e1926a1cf028d782d9dae31c43f2ba44b
Andrew Booker <improvizone@gmail.com>
2022-01-02 20:22:15 +0000

    finishing minimal new fader

--- fader.py
+++ fader.py
@@ -1,26 +1,41 @@
 
+from enum import Enum
+
+class Action(Enum):
+    Nothing = 0
+    Up = 1
+    Down = -1
+
 class FadeUpDown():
-    def __init__(self, rampTime, startTime, upTime, sampleRate):
+    def __init__(self, initBlockTime, rampTime, startTime, upTime, sampleRate):
         self.sampleRate = sampleRate
         self.rampTime = rampTime
         self.startUpTime = startTime
         self.pos = 0
+        self.startUpPos = int((startTime) * sampleRate)
         self.startDownPos = int((rampTime + upTime) * self.sampleRate)
-        self.isDown = False
+        self.action = Action.Nothing
 
     def apply(self, to):
+        if self.action == Action.Nothing and self.pos == self.startUpPos:
+            self.pos = 0
+            self.action = Action.Up
+
         if self.pos == self.startDownPos:
             self.pos = 1
-            self.isDown = True
-        coeff = 1.0 * self.pos / (self.rampTime * self.sampleRate)
-        self.pos += 1
-        if coeff > 1.0:
-            if not self.isDown:
-                return to
-            else:
-                coeff = 1.0
+            self.action = Action.Down
 
-        if self.isDown:
+        coeff = 1.0 * self.pos / (self.rampTime * self.sampleRate)
+
+        self.pos += 1
+        if self.action == Action.Nothing:
+            return to
+
+        if self.action == Action.Down:
             coeff = 1.0 - coeff
+
+        if coeff > 1.0 or coeff < 0.0:
+            self.action = Action.Nothing
+            coeff = 1.0 if coeff > 1.0 else 0.0
 
         return [s * coeff for s in to]
--- test_fader.py
+++ test_fader.py
@@ -2,22 +2,31 @@ from fader import FadeUpDown
 
 
 def test_fades_up_from_the_beginning_of_a_block():
-    fader = FadeUpDown(rampTime=1.0, startTime=0, upTime=99999, sampleRate=5)
+    fad = FadeUpDown(initBlockTime=0, rampTime=1.0, startTime=0, upTime=99999, sampleRate=5)
 
     b = [[1.0]] * 5
-    assert [fader.apply(s) for s in b] == [[0.0], [0.2], [0.4], [0.6], [0.8]]
+    assert [fad.apply(s) for s in b] == [[0.0], [0.2], [0.4], [0.6], [0.8]]
 
 
 def test_maintains_level_after_fade_up():
-    fader = FadeUpDown(rampTime=1.0, startTime=0, upTime=99999, sampleRate=5)
+    fad = FadeUpDown(initBlockTime=0, rampTime=1.0, startTime=0, upTime=99999, sampleRate=5)
 
-    assert [fader.apply(s) for s in [[1.0]] * 5] == [[0.0], [0.2], [0.4], [0.6], [0.8]]
-    assert [fader.apply(s) for s in [[1.0]] * 5] == [[1.0]] * 5
+    assert [fad.apply(s) for s in [[1.0]] * 5] == [[0.0], [0.2], [0.4], [0.6], [0.8]]
+    assert [fad.apply(s) for s in [[1.0]] * 5] == [[1.0]] * 5
 
 
 def test_fades_down_from_the_beginning_of_a_block():
-    fader = FadeUpDown(rampTime=1.0, startTime=0, upTime=1, sampleRate=5)
+    fad = FadeUpDown(initBlockTime=0, rampTime=1.0, startTime=0, upTime=1, sampleRate=5)
 
-    [fader.apply(s) for s in [[1.0]] * 5]
-    [fader.apply(s) for s in [[1.0]] * 5]
-    assert [fader.apply(s) for s in [[1.0]] * 5] == [[0.8], [0.6], [0.4], [0.19999999999999996], [0.0]]
+    assert [fad.apply(s) for s in [[1.0]] * 5] == [[0.0], [0.2], [0.4], [0.6], [0.8]]
+    assert [fad.apply(s) for s in [[1.0]] * 5] == [[1.0]] * 5
+    assert [fad.apply(s) for s in [[1.0]] * 5] == [[0.8], [0.6], [0.4], [0.19999999999999996], [0.0]]
+
+
+def test_fades_within_blocks():
+    fad = FadeUpDown(initBlockTime=0, rampTime=1.0, startTime=0.5, upTime=1, sampleRate=4)
+    
+    assert [fad.apply(s) for s in [[0.4]] * 4][2:] == [[0.0], [0.1]]
+    assert [fad.apply(s) for s in [[0.4]] * 4] == [[0.2], [0.30000000000000004], [0.4], [0.4]]
+    assert [fad.apply(s) for s in [[0.4]] * 4] == [[0.4], [0.4], [0.30000000000000004], [0.2]]
+    assert [fad.apply(s) for s in [[0.4]] * 4][:2] == [[0.1], [0.0]]
